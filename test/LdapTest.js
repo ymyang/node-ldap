5e80bde7d7703c33091d58a0e83c53c8000fc0bd
ymyang <ymyang@163.com>
2016-06-23 17:29:12 +0800

    ldap auth test

--- test/LdapTest.js
+++ test/LdapTest.js
@@ -6,13 +6,29 @@ var LdapClient = require('../index.js');
 describe('ldap', function() {
     it('searchOU', function(done) {
         var client = new LdapClient({
-            ldapUrl: 'ldap://192.168.1.81:389',
-            userDn: 'administrator@ad.yliyun.com',
+            ldapUrl: 'ldap://192.168.0.30:389',
+            userDn: 'administrator@yliyun.com',
             password: 'yliyun@123'
         });
-        client.searchOU('dc=ad,dc=yliyun,dc=com').then(function(ous) {
+        client.searchOU('dc=yliyun,dc=com').then(function(ous) {
             console.log(ous);
             done();
         }).catch(done);
+    });
+
+    it.only('auth', function(done) {
+        var client = new LdapClient({
+            ldapUrl: 'ldap://192.168.0.30:389',
+            userDn: 'administrator@yliyun.com',
+            password: 'yliyun@123'
+        });
+        client.auth('yang@yliyun.com', 'yliyun@123').then(function() {
+            console.log('auth ok');
+            done();
+        }).catch(function(err) {
+            console.log(JSON.stringify(err));
+            console.error(err);
+            done();
+        });
     });
 });
